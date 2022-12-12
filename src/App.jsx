583d82f7fe848952c43ab78fba7099e7cbaa4eb2
Nitesh Kumar Tudu <ntudu040@gmail.com>
2022-12-12 16:56:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
+import './App.css'
+
+function App() {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0
+  })
+
+  const [cursorVariant, setCursorVariant] = useState('default')
+
+  useEffect(() => {
+
+    const mouseMove = (e) => {
+      setMousePosition({x: e.clientX, y: e.clientY})
+    }
+    window.addEventListener('mousemove', mouseMove)
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove)
+    }
+  },[])
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16
+    },
+    text: {
+      height: 150,
+      width: 150,
+      x: mousePosition.x - 75,
+      y: mousePosition.y - 75,
+      backgroundColor: 'yellow',
+      mixBlendMode: 'difference',
+    }
+  }
+
+  const textEnter = () => setCursorVariant('text')
+  const textLeave = () => setCursorVariant('default')
+
+  return (
+    <div className="App">
+      <h1 onMouseEnter={textEnter} onMouseLeave={textLeave} className="title">Hello World</h1>
+      <motion.div 
+      variants={variants}
+      animate={cursorVariant}
+      className="cursor"/>
+    </div>
+  )
+}
+
+export default App
